ef521e721836b6d203bd7cb0e62f2f02b4c5d3c1
joaovsz <joaovitor1713coin@gmail.com>
2024-06-08 20:12:01 -0300

    first commit

---
+++ src/api/firebase.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0SJBhYKYbqThEm8an2SL7TYnwV-J8Jc0",
+  authDomain: "swiftquote-fcc03.firebaseapp.com",
+  projectId: "swiftquote-fcc03",
+  storageBucket: "swiftquote-fcc03.appspot.com",
+  messagingSenderId: "772633274090",
+  appId: "1:772633274090:web:1e4dcc3e6315c2e9fa1993"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
